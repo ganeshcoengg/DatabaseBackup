aa2d4859a005518f4ddf19bfd9b1f1653dfc18fd
Ganesh Vahinde <ganeshcoengg@gmail.com>
2018-09-22 17:29:43 +0530

    Code Optimized

--- GetDatabseBackup.ps1
+++ GetDatabseBackup.ps1
@@ -5,13 +5,17 @@ $ADMINDATABASE = $JsonObject.ADMINDB
 $SADATABASE = $JsonObject.SADB
 $STDATABASE = $JsonObject.STDB
 $SWDATABASE = $JsonObject.SWDB
-$BackupPath	=	$JsonObject.DatabaseBackupPath	
+$BackupPath	=	$JsonObject.DatabaseBackupPath.Path	
 $HostName = $JsonObject.MySQLServer
 $password = $JsonObject.MySQLPassword
 $userName = $JsonObject.MySQLUserName
 $logpath = Get-Location
 $logfile = $logpath.Path+"\"+((Get-Date).ToString('MMdd'))+'DatabaseBackupLogs.txt'
 $isTriggers = $JsonObject.WithTrigger.isTrigger
+$user = $userName.UserName	    
+$pw = $password.Password
+$pass = "-p$pw"
+$DBServer = $HostName.DBHost
 function LogWrite {
     param ([String]$logString)
     $time = Get-Date
@@ -21,21 +25,18 @@ function LogWrite {
 
 LogWrite "Start ==> Database Backup Started!" 
 Function GetDatabaseBackup {
-    Param([string] $DBServer,
-            [string] $DBName,
-            [string] $Backuppath
-            )
-    $user = $userName.UserName		    
-    $pw = $password.Password
-    $pass = "-p$pw"
+    Param([string] $DBName
+    )
+    
+
     $date = ((Get-Date).ToString('yyyyMMddHHmm'))
     $separater = $date+'_'
     Write-Host "$DBName Backup Started..." -NoNewline
     if($isTriggers -eq "YES"){
-    $ErrorLog = & cmd.exe /c "mysqldump -h $DBServer -u $user $pass --databases $DBName > $Backuppath$separater$DBName.sql"
+        $ErrorLog = & cmd.exe /c "mysqldump -h $DBServer -u $user $pass --databases $DBName > $Backuppath$separater$DBName.sql"
     }
     else {
-        $ErrorLog = & cmd.exe /c "mysqldump -h $DBServer -u $user $pass --databases $DBName --skip-triggers > $Backuppath$separater$DBName.sql"        
+        $ErrorLog = & cmd.exe /c "mysqldump -h $DBServer -u $user $pass --databases $DBName --skip-triggers > $Backuppath$separater$DBName.sql"
     }
 
     #check status
@@ -46,7 +47,7 @@ Function GetDatabaseBackup {
     else{
         $ErrorLog= $Error[0].Exception.Message 
         Write-Host "$DBName DATABASE BACKUP FAILED" -ForegroundColor Red
-        LogWrite "ERROR :- ADMIN DATABASE BACKUP HAS BEEN FAILED - $ErrorLog !!"
+        LogWrite "ERROR :- $DBName DATABASE BACKUP HAS BEEN FAILED - $ErrorLog !!"
     }
 }	
 
@@ -54,7 +55,7 @@ function ConvertToRAR {
     #Set rar path 
     if (Test-Path -path  "C:\Program Files (x86)\WinRAR\Rar.exe"){
         $rar = "C:\Program Files (x86)\WinRAR\Rar.exe"
-    }  
+    }
     else{
         $rar = "C:\Program Files\WinRAR\Rar.exe"
     }
@@ -68,34 +69,22 @@ function ConvertToRAR {
 
 # RUN Admin DB backup Script 
 foreach($AdminDatabaseName in $ADMINDATABASE){		
-    $ADMIN = $($AdminDatabaseName.ADMIN)
-    $DBServer = $($HostName.DBHost)
-    $Backuppath=$JsonObject.DatabaseBackupPath.path
-    GetDatabaseBackup $DBServer $ADMIN  $Backuppath
+    GetDatabaseBackup $AdminDatabaseName.ADMIN
 }
 
 # RUN SA DB backup Script 
 foreach($SADatabaseName in $SADATABASE){		
-    $SA = $($SADatabaseName.SA)
-    $DBServer = $($HostName.DBHost)
-    $Backuppath=$JsonObject.DatabaseBackupPath.path
-    GetDatabaseBackup $DBServer $SA  $Backuppath
+    GetDatabaseBackup $SADatabaseName.SA
 }
 
 # RUN ST DB backup Script 
 foreach($STDatabaseName in $STDATABASE){
-    $ST = $($STDatabaseName.ST)
-    $DBServer = $($HostName.DBHost)
-    $Backuppath=$JsonObject.DatabaseBackupPath.path
-    GetDatabaseBackup $DBServer $ST  $Backuppath
+    GetDatabaseBackup $STDatabaseName.ST
 }
 
 # RUN SW DB backup Script 
 foreach($SWDatabaseName in $SWDATABASE){
-    $SW = $($SWDatabaseName.SW)
-    $DBServer = $($HostName.DBHost)
-    $Backuppath=$JsonObject.DatabaseBackupPath.path
-    GetDatabaseBackup $DBServer $SW  $Backuppath
+    GetDatabaseBackup $SWDatabaseName.SW
 }
 # Get the RAR file 
 Write-Host "Creating RAR..." -NoNewline
